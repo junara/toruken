1976f86ddc3995f7321a532d14ef11449633d96e
junara <tepada@yahoo.co.jp>
2016-04-18 10:12:21 +0000

    toriaezu

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,13 +22,19 @@ class UsersController < ApplicationController
     end
   end
 
-  def graph 
+  def graph
+    if params[:format]
+      format = params[:format]
+    else
+      format = '体重'
+    end
+#    format ='体重'
     @user = User.find(params[:id])
     @photos = @user.photos.order(item_date: :desc)
 #       .paginate(:page => params[:page], :per_page => 5) #will_paginateを使用し、5投稿毎にページ訳
       # おためし
 #    @chart_data = [['2014-04-01', 60], ['2014-04-02', 65], ['2014-04-03', 64]]
-    @chart_data = @photos.where(item_name: '体重').order('item_date ASC').group(:item_date).average('item_value')
+    @chart_data = @photos.where(item_name: format).order('item_date ASC').group(:item_date).average('item_value')
     #binding.pry
     
   end
