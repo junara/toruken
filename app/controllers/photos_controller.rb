8353a347435d709a14bbe66fea6a7f6ac0708c6b
junara <tepada@yahoo.co.jp>
2016-04-20 04:26:58 +0000

    gogle vision

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,10 +2,16 @@ class PhotosController < ApplicationController
   before_action :logged_in_user, only: [:create]
   before_action :set_photo , only: [:edit, :update, :destroy]
   include CarrierwaveBase64Uploader
+  require 'httpclient'
   
   def create
+#    gbapi="AIzaSyDPrOSfTi0GqbeDzMHQGv-A0c2cwnuuMo4"
+    gbapi="AIzaSyDPrOSfTi0GqbeDzMHQGv-A0c2cwnuuMo4"
+#    @endpoint_uri = "https://vision.googleapis.com/v1/images:annotate?key=#{ENV['SERVER_KEY']}"
+    @endpoint_uri = "https://vision.googleapis.com/v1/images:annotate?key=#{gbapi}"
+
     @photo = current_user.photos.build(photo_params)
-    # binding.pry
+    @photo[:content]= EasyTranslate.translate(gvrequest(params[:photo][:content]), :from => :en, :to => :ja, :key => gbapi)
     if @photo.save
       flash[:success] = "Photo created!"
       redirect_to root_url
@@ -47,5 +53,32 @@ class PhotosController < ApplicationController
     @photo = Photo.find(params[:id])
   end
 
+  def gvrequest(base64data)
+    http_client = HTTPClient.new
+    content = base64data
+    response = http_client.post_content(@endpoint_uri, request_json(content), 'Content-Type' => 'application/json')
+    result_parse(response)
+    
+  end
 
+  def request_json(content)
+    {
+      requests: [{
+        image: {
+          content: content
+        },
+        features: [{
+          type: "LABEL_DETECTION",
+          maxResults: 10
+        }]
+      }]
+    }.to_json
+  end
+
+  def result_parse(response)
+    result = JSON.parse(response)['responses'].first
+      label = result['labelAnnotations'].first
+      result = label['description']
+    result
+  end
 end
