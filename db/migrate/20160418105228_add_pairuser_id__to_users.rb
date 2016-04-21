ab179f07a21e094ffa08343169d57237a39e2260
junara <tepada@yahoo.co.jp>
2016-04-21 06:15:06 +0000

    dbmigration foreign key

--- db/migrate/20160418105228_add_pairuser_id__to_users.rb
+++ db/migrate/20160418105228_add_pairuser_id__to_users.rb
@@ -1,5 +1,5 @@
 class AddPairuserIdToUsers < ActiveRecord::Migration
   def change
-    add_reference :users, :pairuser, index: true, foreign_key: true
+    add_reference :users, :pairuser, index: true
   end
 end
